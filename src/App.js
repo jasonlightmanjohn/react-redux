71ec03ddf1881021a718667791d006da606a2101
jasonxjung <jasonjung.xc@gmail.com>
2020-04-21 11:48:52 +0300

    complete PostForm component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 //import logo from './logo.svg';
 import Posts from './components/Posts';
+import PostForm from './components/PostForm';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <PostForm />
+      <hr />
       <Posts />
+      <hr />
     </div>
   );
 }
